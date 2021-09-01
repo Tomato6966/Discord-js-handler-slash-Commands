4383a8bfc50084e52a07314c962094e9b94def7b
Tomato6966 <68145571+Tomato6966@users.noreply.github.com>
2021-09-01 18:26:50 +0200

    Add files via upload

--- handlers/slashCommands.js
+++ handlers/slashCommands.js
@@ -1,7 +1,5 @@
 const { readdirSync, lstatSync } = require("fs");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 const config = require("../botconfig/config.json");
 const dirSetup = config.slashCommandsDirs;
 module.exports = (client) => {
@@ -81,32 +79,32 @@ module.exports = (client) => {
 						if(pull.options && pull.options.length > 0){
 							for(const option of pull.options){
 								if(option.User && option.User.name && option.User.description){
-									subcommand.addUserOption((op) =>
+									Command.addUserOption((op) =>
 										op.setName(String(option.User.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.User.description).setRequired(option.User.required)
 									)
 								} else if(option.Integer && option.Integer.name && option.Integer.description){
-									subcommand.addIntegerOption((op) =>
+									Command.addIntegerOption((op) =>
 										op.setName(String(option.Integer.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Integer.description).setRequired(option.Integer.required)
 									)
 								} else if(option.String && option.String.name && option.String.description){
-									subcommand.addStringOption((op) =>
+									Command.addStringOption((op) =>
 										op.setName(String(option.String.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.String.description).setRequired(option.String.required)
 									)
 								} else if(option.Channel && option.Channel.name && option.Channel.description){
-									subcommand.addChannelOption((op) =>
+									Command.addChannelOption((op) =>
 										op.setName(String(option.Channel.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Channel.description).setRequired(option.Channel.required)
 									)
 								} else if(option.Role && option.Role.name && option.Role.description){
-									subcommand.addRoleOption((op) =>
+									Command.addRoleOption((op) =>
 										op.setName(String(option.Role.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.Role.description).setRequired(option.Role.required)
 									)
 								} else if(option.StringChoices && option.StringChoices.name && option.StringChoices.description && option.StringChoices.choices && option.StringChoices.choices.length > 0){
-									subcommand.addStringOption((op) =>
+									Command.addStringOption((op) =>
 										op.setName(String(option.StringChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.StringChoices.description).setRequired(option.StringChoices.required)
 										.addChoices(option.StringChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),String(c[1])] )),
 									)
 								} else if(option.IntChoices && option.IntChoices.name && option.IntChoices.description && option.IntChoices.choices && option.IntChoices.choices.length > 0){
-									subcommand.addStringOption((op) =>
+									Command.addStringOption((op) =>
 										op.setName(String(option.IntChoices.name).replace(/\s+/g, '_').toLowerCase()).setDescription(option.IntChoices.description).setRequired(option.IntChoices.required)
 										.addChoices(option.IntChoices.choices.map(c=> [String(c[0]).replace(/\s+/g, '_').toLowerCase(),parseInt(c[1])] )),
 									)
@@ -126,16 +124,37 @@ module.exports = (client) => {
         
 		//Once the Bot is ready, add all Slas Commands to each guild
 		client.on("ready", () => {
-			const rest = new REST({ version: '9' }).setToken(config.token);
-			client.guilds.cache.map(g => g.id).forEach((gId) => {
-				rest.put(Routes.applicationGuildCommands(client.user.id, gId), { body: allCommands });
-			})
-			console.log(`${client.slashCommands.size} slashCommands Loaded`.brightGreen);
+			if(config.loadSlashsGlobal){
+				client.application.commands.set(allCommands)
+				.then(slashCommandsData => {
+					console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for all: ${`All possible Guilds`.underline}`.brightGreen); 
+					console.log(`Because u are Using Global Settings, it can take up to 1 hour until the Commands are changed!`.bold.yellow)
+				}).catch((e)=>console.log(e));
+			} else {
+				client.guilds.cache.map(g => g).forEach((guild) => {
+					try{
+						guild.commands.set(allCommands)
+						.then(slashCommandsData => {
+							console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for: ${`${guild.name}`.underline}`.brightGreen); 
+						}).catch((e)=>console.log(e));
+					}catch (e){
+						console.log(String(e).grey)
+					}
+				});
+			}
 		})
+		//DISABLE WHEN USING GLOBAL!
 		client.on("guildCreate", (guild) => {
-			const rest = new REST({ version: '9' }).setToken(config.token);
-			rest.put(Routes.applicationGuildCommands(client.user.id, guild.id), { body: allCommands });
-			console.log(`${client.slashCommands.size} slashCommands Loaded for ${guild.name}`.brightGreen);
+			try{
+				if(!config.loadSlashsGlobal){
+					guild.commands.set(allCommands)
+						.then(slashCommandsData => {
+							console.log(`${slashCommandsData.size} slashCommands ${`(With ${slashCommandsData.map(d => d.options).flat().length} Subcommands)`.green} Loaded for: ${`${guild.name}`.underline}`.brightGreen); 
+						}).catch((e)=>console.log(e));
+				}
+			}catch (e){
+				console.log(String(e).grey)
+			}
 		})
 		
     } catch (e) {
